264fe7a39aa21e5b07a306826c2c41d2ada0f466
Peixin Liu <liupeixin50@gmail.com>
2026-06-06 16:56:26 +0100

    fix a main.c error

--- main/main.c
+++ main/main.c
@@ -5,8 +5,10 @@
 #include "esp_log.h"
 
 extern void task_max30102(void *vparameter);
+extern void task_ad8232(void *vparameter);
 void app_main(void)
 {    
     xTaskCreate(task_max30102, "task_max30102", 4096*2, NULL, 5, NULL);
+    xTaskCreate(task_ad8232, "task_ad8232", 4096*2, NULL, 5, NULL);
    
 }
