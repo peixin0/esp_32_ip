986be217071d760e20efb17b482ee73332ba7b4a
Peixin Liu <liupeixin50@gmail.com>
2026-07-11 20:10:22 +0100

    delete the wifi task

--- main/main.c
+++ main/main.c
@@ -9,7 +9,6 @@
 
 extern void task_max30102(void *vparameter);
 extern void task_ad8232(void *vparameter);
-extern void task_wifi_station(void *vparameter);
 extern void task_tb_mqtt(void *vparameter);
 void app_main(void)
 {   
@@ -24,7 +23,6 @@ void app_main(void)
 
     xTaskCreate(task_max30102, "task_max30102", 4096*2, NULL, 5, NULL);
     // xTaskCreate(task_ad8232, "task_ad8232", 4096*2, NULL, 5, NULL);
-    xTaskCreate(task_wifi_station, "task_wifi_station", 4096*2, NULL, 5, NULL);
     xTaskCreate(task_tb_mqtt, "task_tb_mqtt", 4096*2, NULL, 5, NULL);
     // if (timebase_sync(100000) != ESP_OK)
     // {
